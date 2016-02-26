8d675f008a55c0821c9131c1cdee51a298ebc34c
TESLA <57leonardo@gmail.com>
2016-02-26 14:05:40 -0500

    progress with several methods

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "display.h"
 #include "draw.h"
 #include "matrix.h"
-#include "parser.h"
+// #include "parser.h"
 
 int main() {
 
@@ -17,7 +17,13 @@ int main() {
   edges = new_matrix(4, 4);
   transform = new_matrix(4, 4);
 
-  
+  // testing print
+  ident(edges);
+  print_matrix(edges);
+  scalar_mult(9, edges);
+  printf("\n");
+  print_matrix(edges);
+
   free_matrix( transform );
   free_matrix( edges );
-}  
+}
