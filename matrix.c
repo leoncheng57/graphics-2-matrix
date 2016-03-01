cd2c417301c79fbf85f06c6a01ec9c89c20cd34c
TESLA <57leonardo@gmail.com>
2016-02-29 23:49:40 -0500

    think i got matrix mult to workgit add -A :)

--- matrix.c
+++ matrix.c
@@ -142,22 +142,27 @@ void matrix_mult(struct matrix *a, struct matrix *b) {
     double * row = a->m[h];
 
     for (i=0;i<b->rows;i++){
-      int sum; //sum of row*col
+      double sum = 0; //sum of row*col
       for (j=0; j<b->cols;j++){
-        sum += row[j] * b->m[i][j];
-        printf("row[%f]: %f \n", j, row[j]);
-        printf("b->m[%f][%f]: %f \n", i, j, b->m[i][j]);
+        printf("row[%d]: %f \n", j, row[j]);
+        printf("b->m[%d][%d]: %f \n", i, j, b->m[i][j]);
         printf("row[j] * b->m[i][j]: %f\n", row[j] * b->m[i][j]);
+        sum += row[j] * b->m[i][j];
         //h, for the row, stays the same
         //i, for the col, stays the same
       }
       //one complete col
       result->m[h][i] = sum;
-      printf("sum=m[%f][%f]=%f\n",h, i, sum );
+      printf("sum=m[%d][%d]=%f\n", h, i, sum );
       printf("\n");
     }
   }
-  b=result;
+  //Copy result matrix to b
+  for(i=0; i<b->rows; i++){
+    for(j=0; j<b->cols; j++){
+      b->m[i][j] = result->m[i][j];
+    }
+  }
 }
 
 
