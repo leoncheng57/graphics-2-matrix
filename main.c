b79702462969516b9b2e4296a8edad333dd050b0
TESLA <57leonardo@gmail.com>
2016-03-06 17:36:45 -0500

    tested eveyrtrhing :)

--- main.c
+++ main.c
@@ -24,18 +24,30 @@ int main() {
   c.blue = 0;
 
   edges = new_matrix(4, 4);
-  add_edge(edges, 100, 100, 100, 200, 200, 200);
-  print_matrix(edges);
+  add_edge(edges, 100, 100, 100, 200, 200, 200); //first line
   draw_lines(edges, s, c);
 
   c.blue = MAX_COLOR;
-  transform = make_translate(200, 200, 0);
-  print_matrix(transform);
+  transform = make_translate(200, 200, 0); //transform
   matrix_mult(transform, edges);
-  print_matrix(edges);
-
   draw_lines(edges, s, c);
 
+  c.red = MAX_COLOR;
+  c.blue = 0;
+  scalar_mult(0.5, edges); //scaling
+  draw_lines(edges, s, c);
+
+  c.green = 0;
+  c.blue = 0;
+  transform = make_scale(0.25, 1, 1);
+  matrix_mult(transform, edges);
+  draw_lines(edges, s, c);
+  
+  c.green = MAX_COLOR;
+  c.blue = MAX_COLOR;
+  transform = make_rotZ(-20);
+  matrix_mult(transform, edges);
+  draw_lines(edges, s, c);
 
   //end testing
 
